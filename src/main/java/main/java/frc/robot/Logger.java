86678cde8235fd10bdf171cf0b53c321d00eee54
Denis Rheault <rheault.denis@gmail.com>
2018-12-10 20:07:24 -0500

    Remove file delete in Logger.java

--- src/main/java/main/java/frc/robot/Logger.java
+++ src/main/java/main/java/frc/robot/Logger.java
@@ -29,30 +29,16 @@ public class Logger {
     
 	public void println(String x) {
 	   System.out.println(x);
-	   try {
-		   if (logFile.exists()) {
-			   logFile.delete();
-		   }
-		   FileWriter logFileFw = new FileWriter(logFile, true);
-		   logFileFw.write(x); 
-		   logFileFw.close();
-	   } catch (IOException e) {
-		   e.printStackTrace();	   
-	   }
+	   FileWriter logFileFw = new FileWriter(logFile, true);
+	   logFileFw.write(x+'\n'); 
+	   logFileFw.close();
     }
 	
 	public void addRecord() {
-	   try {
-		   if (recFile.exists()) {
-			   recFile.delete();
-		   }
-		   FileWriter recFileFw = new FileWriter(recFile, true);
-		   recFileFw.write(recLine);
-		   recFileFw.close();
-		   recLine = ""; // start a new line
-	   } catch (IOException e) {
-		   e.printStackTrace();	   
-	   }
+	   FileWriter recFileFw = new FileWriter(recFile, true);
+	   recFileFw.write(recLine+'\n');
+	   recFileFw.close();
+	   recLine = ""; // start a new line
    }
 
 	public static void appendRecord(String x) {
